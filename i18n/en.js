3e7ae304a5801b96eb722e5ab8ac814f89142382
Silvestre Huens <s.huens@gmail.com>
2012-03-13 04:03:31 -0300

    basic plugin data and funcionality (create and delete tasks, prepopulation from an existing textarea element)

---
+++ i18n/en.js
@@ -0,0 +1,6 @@
+tp_i18n = {
+    add:'Add',
+    add_task:'Add a task',
+    delete_task:'Delete task',
+    edit_task:'Edit task'
+}
---
+++ i18n/es.js
@@ -0,0 +1,6 @@
+tp_i18n = {
+    add:'Agregar',
+    add_task:'Agregar una nueva tarea',
+    delete_task:'Borrar tarea',
+    edit_task:'Editar tarea'
+}
